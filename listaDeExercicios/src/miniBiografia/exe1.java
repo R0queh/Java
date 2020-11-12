be76496a57d4e7b68cef226c4e3fa877a7fff4fc
Bruno Santos <brunosrdeoliveira@gmail.com>
2020-11-12 15:31:42 -0300

    adicionando listas de exercícios ao github

---
+++ listaDeExercicios/src/miniBiografia/exe1.java
@@ -0,0 +1,19 @@
+package miniBiografia;
+
+public class exe1 {
+	public static void main(String args[]) {
+		String eu;
+				eu = ("Prazer sou o bruno, tenho 19 anos e ouço muuuuita música,"
+				+ "\nquando eu estou tranquilo normalmente eu curto assistir ONE PIECE"
+				+ "\ne fazer churrasco, como disse acima gosto muito de música e "
+				+ "\nsou muito eclético meeesmo o que o dj tocar eu estou dançando e "
+				+ "\ncá entre nós o pai dança muuuuuuito (mal hahahaha), jogo um fut de"
+				+ "\nsexta e estou ai atrás de trabalhar na área de tecnologia"
+				+ "\nahh lembrei eu leio também livros que me chamam atenção são os "
+				+ "\nque falam sobre comportamento, os nerds, os de biografia e os de "
+				+ "\npsicologia, quase me esqueci sou muito nerd então gosto muito de herois"
+				+ "\nhomem aranha (para os intimos miranha) e lanterna verde(ou o asa noturna)"
+				+ "\nsão os preferidos de cada uma das grandes marcas de quadrinhos.");
+		System.out.println(eu);
+	}
+}
